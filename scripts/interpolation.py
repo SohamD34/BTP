47d1bfb97a2c0080a0ed9aba506a7cb824a0981a
SohamD34 <deshmukh.2@iitj.ac.in>
2024-08-28 20:06:32 +0530

    Updated environment

---
+++ scripts/interpolation.py
@@ -0,0 +1,82 @@
+!pip install smogn
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+import warnings
+import smogn
+
+warnings.filterwarnings('ignore')
+
+data1 = pd.read_csv('/kaggle/input/btp-analyte-data/Analyte1.csv')
+data1 = data1.drop(['Unnamed: 0'], axis=1)
+data1['log'] = np.log(data1['CFU/mL'])
+
+
+data2 = pd.read_csv('/kaggle/input/btp-analyte-data/Analyte2.csv')
+data2 = data2.drop(['Unnamed: 0'], axis=1)
+data2['log'] = np.log(data2['CFU/mL'])
+
+
+data3 = pd.read_csv('/kaggle/input/btp-analyte-data/Analyte3.csv')
+data3 = data3.drop(['Unnamed: 0'], axis=1)
+data3['log'] = np.log(data3['CFU/mL'])
+
+
+''' APPLYING SMOTE '''
+
+ext_data1 = data1.copy()
+
+for i in range(5):
+    smogn_sensor = smogn.smoter(
+        data = data1, 
+        y = "CFU/mL",
+        under_samp=False,
+        drop_na_col=True,
+        drop_na_row=True,
+        samp_method="extreme",
+        k = 5
+    )
+    ext_data1 = pd.concat([ext_data1, smogn_sensor])
+    ext_data1 = ext_data1.reset_index(drop=True)
+    print(len(ext_data1))
+    
+
+ext_data2 = data2.copy()
+
+for i in range(5):
+    smogn_sensor = smogn.smoter(
+        data = data2, 
+        y = "CFU/mL",
+        under_samp=False,
+        drop_na_col=True,
+        drop_na_row=True,
+        samp_method="extreme",
+        k = 5
+    )
+    ext_data2 = pd.concat([ext_data2, smogn_sensor])
+    ext_data2 = ext_data2.reset_index(drop=True)
+    print(len(ext_data2))
+    
+
+ext_data3 = data3.copy()
+
+for i in range(5):
+    smogn_sensor = smogn.smoter(
+        data = data3, 
+        y = "CFU/mL",
+        under_samp=False,
+        drop_na_col=True,
+        drop_na_row=True,
+        samp_method="extreme",
+        k = 5
+    )
+    ext_data3 = pd.concat([ext_data3, smogn_sensor])
+    ext_data3 = ext_data3.reset_index(drop=True)
+    print(len(ext_data3))
+    
+
+ext_data1.to_csv('../data/Analyte1.csv')
+ext_data2.to_csv('../data/Analyte2.csv')
+ext_data3.to_csv('../data/Analyte3.csv')
