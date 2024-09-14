c88bc58d4f2d7b7251fc07c4239a1579a26a60dd
SohamD34 <deshmukh.2@iitj.ac.in>
2024-09-14 10:00:30 +0530

    clustering + preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,6 +1,6 @@
 import os
 import sys
-os.chdir('/media/soham/New Volume/GitHub/BTP/')
+os.chdir('/home/soham/Desktop/GitHub/BTP/')
 sys.path.append(os.getcwd())
 
 from utils.preprocessing import slice_datasets, drop_columns, rename_columns
@@ -13,6 +13,13 @@ analyte_1, analyte_2, analyte_3 = slice_datasets(data)
 [analyte_1, analyte_2, analyte_3] = drop_columns([analyte_1, analyte_2, analyte_3])
 [analyte_1, analyte_2, analyte_3] = rename_columns([analyte_1, analyte_2, analyte_3])
 
-analyte_1.to_csv('data/Unsampled_Analyte1.csv')
-analyte_2.to_csv('data/Unsampled_Analyte2.csv')
-analyte_3.to_csv('data/Unsampled_Analyte3.csv')
+
+# ---------- Manage 0 concentration data ------------------ #
+
+for a in [analyte_1, analyte_2, analyte_3]:
+    a['CFU/mL'] = a['CFU/mL'].replace(0, 1)
+
+analyte_1.to_csv('data/Unsampled_Analyte1.csv', index=False)
+analyte_2.to_csv('data/Unsampled_Analyte2.csv', index=False)
+analyte_3.to_csv('data/Unsampled_Analyte3.csv', index=False)
+
