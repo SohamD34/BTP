8ef70e17f5ad91501cd214dd53bc03ce6f1439a4
SohamD34 <deshmukh.2@iitj.ac.in>
2024-10-28 11:09:42 +0530

    Added plotting utilities

--- scripts/Mixture + Analytes/4 Mixtures/utils.py
+++ scripts/Mixture + Analytes/4 Mixtures/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import re
 
 
 def add_noise(data, labels, noise_level, target_size=350):
@@ -29,3 +31,81 @@ def add_noise(data, labels, noise_level, target_size=350):
     combined_labels = combined_labels.iloc[:target_size]
 
     return combined_data, combined_labels
+
+
+
+def plot_pca_decision_boundary(pca_data, model, label_mapping):
+
+    fig, ax = plt.subplots(1, 2, figsize=(15, 4))
+    x_min, x_max = pca_data['PC1'].min() - 50, pca_data['PC1'].max() + 50
+    y_min, y_max = pca_data['PC2'].min() - 50, pca_data['PC2'].max() + 50
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 500), np.linspace(y_min, y_max, 500))
+
+    X = pca_data[['PC1','PC2']]
+    Y = pca_data['Labels'].map({v: k for k, v in label_mapping.items()})
+
+    Z = model.predict(np.c_[xx.ravel().astype(float), yy.ravel().astype(float)])
+    Z = Z.reshape(xx.shape)
+
+    predictions = model.predict(X)
+
+    # PLOTTING THE DECISION BOUNDARY 
+
+    ax[0].contourf(xx, yy, Z, alpha=0.2, cmap='viridis')
+    scatter = ax[0].scatter(pca_data['PC1'], pca_data['PC2'], c=Y, cmap='viridis')
+    # legend0 = ax[0].legend(*scatter.legend_elements(), title="Classes")
+
+    handles, labels = scatter.legend_elements()
+    labels = [label_mapping[int(re.search(r'\d+', label).group())] for label in labels]
+    ax[0].legend(handles, labels, title="Classes")
+    ax[0].set_title('True Class Labels')
+
+    ax[1].contourf(xx, yy, Z, alpha=0.2, cmap='viridis')
+    scatter = ax[1].scatter(pca_data['PC1'], pca_data['PC2'], c=predictions, cmap='viridis')
+    legend1 = ax[1].legend(*scatter.legend_elements(), title="Classes")
+
+    handles, labels = scatter.legend_elements()
+    labels = [label_mapping[int(re.search(r'\d+', label).group())] for label in labels]
+    ax[1].legend(handles, labels, title="Classes")
+    ax[1].set_title('Predicted Class Labels and Decision Boundary')
+
+    plt.show()
+
+
+
+def plot_lda_decision_boundary(lda_data, model, label_mapping):
+
+    fig, ax = plt.subplots(1, 2, figsize=(15, 4))
+    x_min, x_max = lda_data['LD1'].min() - 5, lda_data['LD1'].max() + 5
+    y_min, y_max = lda_data['LD2'].min() - 5, lda_data['LD2'].max() + 5
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 500), np.linspace(y_min, y_max, 500))
+
+    X = lda_data[['LD1','LD2']]
+    Y = lda_data['Labels'].map({v: k for k, v in label_mapping.items()})
+
+    Z = model.predict(np.c_[xx.ravel().astype(float), yy.ravel().astype(float)])
+    Z = Z.reshape(xx.shape)
+
+    predictions = model.predict(X)
+
+    # PLOTTING THE DECISION BOUNDARY 
+
+    ax[0].contourf(xx, yy, Z, alpha=0.2, cmap='viridis')
+    scatter = ax[0].scatter(lda_data['LD1'], lda_data['LD2'], c=Y, cmap='viridis')
+    # legend0 = ax[0].legend(*scatter.legend_elements(), title="Classes")
+
+    handles, labels = scatter.legend_elements()
+    labels = [label_mapping[int(re.search(r'\d+', label).group())] for label in labels]
+    ax[0].legend(handles, labels, title="Classes")
+    ax[0].set_title('True Class Labels')
+
+    ax[1].contourf(xx, yy, Z, alpha=0.2, cmap='viridis')
+    scatter = ax[1].scatter(lda_data['LD1'], lda_data['LD2'], c=predictions, cmap='viridis')
+    legend1 = ax[1].legend(*scatter.legend_elements(), title="Classes")
+
+    handles, labels = scatter.legend_elements()
+    labels = [label_mapping[int(re.search(r'\d+', label).group())] for label in labels]
+    ax[1].legend(handles, labels, title="Classes")
+    ax[1].set_title('Predicted Class Labels and Decision Boundary')
+
+    plt.show()
